bead4402555562549f77b82dbfbfa5c036dd9b6f
suraj <surajjeswara@yahoo.com>
2020-03-29 05:39:54 +0530

    OTP send over email

--- app/Http/Controllers/API/EmailController.php
+++ app/Http/Controllers/API/EmailController.php
@@ -14,22 +14,24 @@ class EmailController extends Controller
 {
         public function sendOtp($email)
     {
+    	// validate email id
     	$status=0;
     	$data['email']=$email;
     	$data['otp']=rand(1000,9999);
     	// $data['title'] = "This is Test Mail Tuts Make";
 
     	$checkRecentAttempt=otp::where('email',$email)->orderby('created_at','desc')->first();
-		$diff=date_diff(new \DateTime(date('Y-m-d H:i:s e')),$checkRecentAttempt->created_at);
 		// if the otp ages more than 5 in then create a new one else send a status to use the existing
 		$minMins=date('i', mktime(0,0,300));
-		if(isset($checkRecentAttempt))
+		if(isset($checkRecentAttempt)){
+		$diff=date_diff(new \DateTime(date('Y-m-d H:i:s e')),$checkRecentAttempt->created_at);
 			if($diff->format('%i')<=$minMins){
 			$response = [
 		        'message' =>'Please use the OTP you have already received a moment ago',
 		    ];
 		    return response()->json($response, '200');
 			}
+		}
 
         $rgst=new otp;
         $rgst->email=$email;
--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -20,16 +20,17 @@ class EmailController extends Controller
     	// $data['title'] = "This is Test Mail Tuts Make";
 
     	$checkRecentAttempt=otp::where('email',$email)->orderby('created_at','desc')->first();
-		$diff=date_diff(new \DateTime(date('Y-m-d H:i:s e')),$checkRecentAttempt->created_at);
 		// if the otp ages more than 5 in then create a new one else send a status to use the existing
 		$minMins=date('i', mktime(0,0,300));
-		if(isset($checkRecentAttempt))
+		if(isset($checkRecentAttempt)){
+		$diff=date_diff(new \DateTime(date('Y-m-d H:i:s e')),$checkRecentAttempt->created_at);
 			if($diff->format('%i')<=$minMins){
 			$response = [
 		        'message' =>'Please use the OTP you have already received a moment ago',
 		    ];
 		    return response()->json($response, '200');
 			}
+		}
 
         $rgst=new otp;
         $rgst->email=$email;
